df9ae6d17f4c7d9cc517ab80aaa175bfceade26e
WONDERHAO <61196957+wonderhao123@users.noreply.github.com>
2025-12-30 02:59:32 +0800

    Fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/vnc',
+  assetPrefix: '/vnc/',
   images: {
     unoptimized: true, // Required for static export
   },
